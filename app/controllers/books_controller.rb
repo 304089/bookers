5883a8d543cb37b9e6c2248182dc2c6fe1478bc8
304089 <9457imai2@gmail.com>
2021-08-03 10:39:48 +0000

    [Add]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,7 +12,8 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-       redirect_to books_path
+       flash[:notice] = "Book was successfully created."
+       redirect_to book_path(@book.id)
     else
        @books = Book.all.order(id: :DESC)
        render :index
@@ -27,6 +28,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(@book.id)
 
   end
