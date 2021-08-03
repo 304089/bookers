8c832407134112c61480d5a610ec8683db218fab
304089 <9457imai2@gmail.com>
2021-08-03 02:35:17 +0000

    [Update]index-view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,28 @@
 class BooksController < ApplicationController
   
+  def new
+    @book = Book.new
+  end
+  
+  def index
+    @books = Book.all.order(id: :DESC)
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def create
+    
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+
+  def destroy
+  end
+  
 end
